46b0946a885c3da3f81dae10fedd93f319e8f8a7
b00930755 <mcfadden-n6@ulster.ac.uk>
2025-10-06 16:25:30 +0100

    Updated code with a new Member class.

---
+++ Week3-Library/Member.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Library
+{
+    internal class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {MemberId}");
+            Console.WriteLine($"Member name: {Name}");
+            Console.WriteLine($"Member address: {Address}");
+            Console.WriteLine($"Member phone no: {Phone}");
+            Console.WriteLine();
+        }
+    }
+}
