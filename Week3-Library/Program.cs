986ed1ef495ceaac9cfcef8ddfd02fa814ba7c70
b00930755 <mcfadden-n6@ulster.ac.uk>
2025-10-06 14:53:21 +0100

    Created new classes.

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+    }
+    
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        Book book1 = new Book();
+        book1.Title = "C# Methods and classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+        book.DisplayInfo();
+        book1.DisplayInfo();
+
+
+
+    }
+}
